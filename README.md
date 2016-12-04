89b246be6ec6b76b562385d0d91aff79533d6577
Jannis Rosenbaum <j.ro@me.com>
2016-12-04 21:10:40 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NTLMssp-Extract
 A small Python-Script to extract NetNTLMv2 Hashes from NTMLssp-HTTP-Authentications, which were captured in a pcap.
+
+Requires **pyshark** (https://github.com/KimiNewt/pyshark) and **Python 2.7**
+
+#Installation:
+Just clone this repository and run the *ntlmssp_extract.py*  file.
+
+#Usage:
+After executing the python-file, you can choose which output format you want, *0* for (cuda/ocl)[Hashcat](https://hashcat.net/hashcat/) or *1* for [JohnTheRipper](http://www.openwall.com/john/)
+Then you are able to enter the full path of your capture-file (if you want to speed up the extraction, you should remove all the non related packages from the capture before you run NTLMssp-Extract).
+    
+###Todos:
+ *	pass path and format via command line parameters
+ * filter out corrupt/invalid hashes & packages
+
+
+###Contributing:
+While I think the script does the job, which it was written for, quite good, I'm always open to new ideas and/or pull requests & issues!
---
+++ ntlmssp_extract.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pyshark
+output_format = input("Output-Format (0 => Hashcat; 1 => JTR) ")
+path = raw_input("Path for pcap > ")
+
+cap = pyshark.FileCapture(path,display_filter="ntlmssp && http")
+all_cor = {}
+for pack in cap:
+    try:
+        if(pack.http):
+            if(pack.http.ntlmssp_messagetype == "0x00000001"):
+                if(all_cor.has_key(pack.tcp.stream)):
+                    all_cor[pack.tcp.stream]["hello"]=pack
+                else:
+                    all_cor[pack.tcp.stream] = {"hello":pack}
+            elif(pack.http.ntlmssp_messagetype == "0x00000002"):
+                if(all_cor.has_key(pack.tcp.stream)):
+                    all_cor[pack.tcp.stream]["challenge"]=pack
+            elif(pack.http.ntlmssp_messagetype == "0x00000003"):
+                if(all_cor.has_key(pack.tcp.stream)):
+                    all_cor[pack.tcp.stream]["auth"]=pack
+    except:
+        pass
+all_cor_keys = all_cor.keys()
+all_cor_keys.sort()
+for k in all_cor_keys:
+    curr_cor = all_cor[k]
+    if(len(curr_cor) < 3):
+        continue
+    try:
+        un = curr_cor["auth"].http.ntlmssp_auth_username
+        ch = curr_cor["challenge"].http.ntlmssp_ntlmserverchallenge.replace(":","")
+        domain = curr_cor["auth"].http.ntlmssp_auth_domain
+        ntlm_all = curr_cor["auth"].http.ntlmssp_auth_ntresponse.replace(":","")
+        ntlm_1 = ntlm_all[0:32]
+        ntlm_2 = ntlm_all[32:]
+        if(output_format):
+            print un+":$NETNTLMv2$"+domain+"$"+ch+"$"+ntlm_1+"$"+ntlm_2
+        else:
+            print un+":::"+ch+":"+ntlm_1+":"+ntlm_2
+        print ""
+    except:
+        pass
+
