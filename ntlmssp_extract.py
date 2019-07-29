dc130d0fa1aee48273969dc4d9a7da9668ecb1b1
Djent- <patrick.hurd.1995@gmail.com>
2019-07-29 14:09:51 -0500

    Update to use python3 and available pyshark

--- ntlmssp_extract.py
+++ ntlmssp_extract.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pyshark
 output_format = input("Output-Format (0 => Hashcat; 1 => JTR) ")
-path = raw_input("Path for pcap > ")
+path = input("Path for pcap > ")
 
 cap = pyshark.FileCapture(path,display_filter="ntlmssp && http")
 all_cor = {}
@@ -10,20 +10,20 @@ for pack in cap:
     try:
         if(pack.http):
             if(pack.http.ntlmssp_messagetype == "0x00000001"):
-                if(all_cor.has_key(pack.tcp.stream)):
+                if(pack.tcp.stream in all_cor):
                     all_cor[pack.tcp.stream]["hello"]=pack
                 else:
                     all_cor[pack.tcp.stream] = {"hello":pack}
             elif(pack.http.ntlmssp_messagetype == "0x00000002"):
-                if(all_cor.has_key(pack.tcp.stream)):
+                if(pack.tcp.stream in all_cor):
                     all_cor[pack.tcp.stream]["challenge"]=pack
             elif(pack.http.ntlmssp_messagetype == "0x00000003"):
-                if(all_cor.has_key(pack.tcp.stream)):
+                if(pack.tcp.stream in all_cor):
                     all_cor[pack.tcp.stream]["auth"]=pack
-    except:
+    except Exception as e:
         pass
 all_cor_keys = all_cor.keys()
-all_cor_keys.sort()
+all_cor_keys = sorted(all_cor_keys)
 for k in all_cor_keys:
     curr_cor = all_cor[k]
     if(len(curr_cor) < 3):
@@ -35,11 +35,9 @@ for k in all_cor_keys:
         ntlm_all = curr_cor["auth"].http.ntlmssp_auth_ntresponse.replace(":","")
         ntlm_1 = ntlm_all[0:32]
         ntlm_2 = ntlm_all[32:]
-        if(output_format):
-            print un+":$NETNTLMv2$"+domain+"$"+ch+"$"+ntlm_1+"$"+ntlm_2
+        if(output_format == '1'):
+            print(un+":$NETNTLMv2$"+domain+"$"+ch+"$"+ntlm_1+"$"+ntlm_2)
         else:
-            print un+":::"+ch+":"+ntlm_1+":"+ntlm_2
-        print ""
+            print(un+"::"+domain+":"+ch+":"+ntlm_1+":"+ntlm_2)
     except:
         pass
-
