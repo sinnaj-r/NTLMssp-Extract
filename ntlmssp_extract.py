e4e5cff331ab0e642dd9cb039bd850569b8eb95a
file-not-found <file-not-found@tritonmail.de>
2020-03-08 20:59:15 +0100

    command line parameters added and minor bugfixing

--- ntlmssp_extract.py
+++ ntlmssp_extract.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pyshark
-output_format = input("Output-Format (0 => Hashcat; 1 => JTR) ")
-path = input("Path for pcap > ")
+import argparse
+
+parser = argparse.ArgumentParser(description="extract NetNTLMv2 hashes from pcap file")
+parser.add_argument("-f", metavar="format", type=int, help="output format (0 => Hashcat; 1 => JTR)")
+parser.add_argument("-p", metavar="pcap", help="pcap file path")
+args = parser.parse_args()
+
+if args.f in [0, 1]:
+    output_format = args.f
+else:
+    output_format = input("Output-Format (0 => Hashcat; 1 => JTR) ")
+
+if args.p:
+    path = args.p
+else:
+    path = input("Path for pcap > ")
 
 cap = pyshark.FileCapture(path,display_filter="ntlmssp")
 all_cor = {}
 for pack in cap:
-    if "HTTP" in str(pack.layers):
+    if "<HTTP " in str(pack.layers):
         data = pack.http
-    elif "SMB" in str(pack.layers):
+    elif "<SMB " in str(pack.layers):
         data = pack.smb
+    elif "<SMB2 " in str(pack.layers):
+        data = pack.smb2
+    if data:
         try:
             if(data.ntlmssp_messagetype == "0x00000002"):
                 if(pack.tcp.stream in all_cor):
@@ -20,7 +37,10 @@ for pack in cap:
             elif(data.ntlmssp_messagetype == "0x00000003"):
                 if(pack.tcp.stream in all_cor):
                     all_cor[pack.tcp.stream]["username"]=data.ntlmssp_auth_username
-                    all_cor[pack.tcp.stream]["domain"]=data.ntlmssp_auth_domain
+                    if data.ntlmssp_auth_domain == "NULL":
+                        all_cor[pack.tcp.stream]["domain"]=""
+                    else:
+                        all_cor[pack.tcp.stream]["domain"]=data.ntlmssp_auth_domain
                     all_cor[pack.tcp.stream]["ntlm_1"]=data.ntlmssp_auth_ntresponse.replace(":","")[0:32]
                     all_cor[pack.tcp.stream]["ntlm_2"]=data.ntlmssp_auth_ntresponse.replace(":","")[32:]
         except Exception as e:
