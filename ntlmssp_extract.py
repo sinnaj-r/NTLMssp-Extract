43e7a52cd5596717c523e413dcf5296d9484f3d9
file-not-found <file-not-found@tritonmail.de>
2020-03-08 20:18:50 +0100

    added SMB to supported protocols

--- ntlmssp_extract.py
+++ ntlmssp_extract.py
@@ -4,37 +4,39 @@ import pyshark
 output_format = input("Output-Format (0 => Hashcat; 1 => JTR) ")
 path = input("Path for pcap > ")
 
-cap = pyshark.FileCapture(path,display_filter="ntlmssp && http")
+cap = pyshark.FileCapture(path,display_filter="ntlmssp")
 all_cor = {}
 for pack in cap:
-    try:
-        if(pack.http):
-            if(pack.http.ntlmssp_messagetype == "0x00000001"):
+    if "HTTP" in str(pack.layers):
+        data = pack.http
+    elif "SMB" in str(pack.layers):
+        data = pack.smb
+        try:
+            if(data.ntlmssp_messagetype == "0x00000002"):
                 if(pack.tcp.stream in all_cor):
-                    all_cor[pack.tcp.stream]["hello"]=pack
+                    all_cor[pack.tcp.stream]["challenge"]=data.ntlmssp_ntlmserverchallenge.replace(":","")
                 else:
-                    all_cor[pack.tcp.stream] = {"hello":pack}
-            elif(pack.http.ntlmssp_messagetype == "0x00000002"):
+                    all_cor[pack.tcp.stream] = {"challenge":data.ntlmssp_ntlmserverchallenge.replace(":","")}
+            elif(data.ntlmssp_messagetype == "0x00000003"):
                 if(pack.tcp.stream in all_cor):
-                    all_cor[pack.tcp.stream]["challenge"]=pack
-            elif(pack.http.ntlmssp_messagetype == "0x00000003"):
-                if(pack.tcp.stream in all_cor):
-                    all_cor[pack.tcp.stream]["auth"]=pack
-    except Exception as e:
-        pass
+                    all_cor[pack.tcp.stream]["username"]=data.ntlmssp_auth_username
+                    all_cor[pack.tcp.stream]["domain"]=data.ntlmssp_auth_domain
+                    all_cor[pack.tcp.stream]["ntlm_1"]=data.ntlmssp_auth_ntresponse.replace(":","")[0:32]
+                    all_cor[pack.tcp.stream]["ntlm_2"]=data.ntlmssp_auth_ntresponse.replace(":","")[32:]
+        except Exception as e:
+            pass
 all_cor_keys = all_cor.keys()
 all_cor_keys = sorted(all_cor_keys)
 for k in all_cor_keys:
     curr_cor = all_cor[k]
-    if(len(curr_cor) < 3):
+    if(len(curr_cor) != 5):
         continue
     try:
-        un = curr_cor["auth"].http.ntlmssp_auth_username
-        ch = curr_cor["challenge"].http.ntlmssp_ntlmserverchallenge.replace(":","")
-        domain = curr_cor["auth"].http.ntlmssp_auth_domain
-        ntlm_all = curr_cor["auth"].http.ntlmssp_auth_ntresponse.replace(":","")
-        ntlm_1 = ntlm_all[0:32]
-        ntlm_2 = ntlm_all[32:]
+        un = curr_cor["username"]
+        ch = curr_cor["challenge"]
+        domain = curr_cor["domain"]
+        ntlm_1 = curr_cor["ntlm_1"]
+        ntlm_2 = curr_cor["ntlm_2"]
         if(output_format == '1'):
             print(un+":$NETNTLMv2$"+domain+"$"+ch+"$"+ntlm_1+"$"+ntlm_2)
         else:
